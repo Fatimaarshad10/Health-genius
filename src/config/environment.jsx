ba8457677747435b3a0efcb4e3f4d4f308941a53
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-05-28 23:13:10 +0500

    Day no 20 28-05-2024 -3

--- src/config/environment.jsx
+++ src/config/environment.jsx
@@ -1,5 +1,5 @@
 let apiEndpoint = "https://health-genius.onrender.com/api/v1/";
 // let apiEndpoint = "http://localhost:4000/api/v1/";
-let socketEndpoint = "https://health-genius.onrender.com/socket.io";
+let socketEndpoint = "wss://health-genius.onrender.com/socket.io";
 // let socketEndpoint = "http://localhost:4000/";
 export {apiEndpoint , socketEndpoint} ;
